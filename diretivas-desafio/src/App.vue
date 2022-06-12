1f9592edba9e88111b49bbd9ea6fd2bd046c8b96
CURSOSDEVS <claudisneibello@hotmail.com>
2022-06-12 00:08:32 -0300

    Finalizando modulo de diretivas e fazendo desafio

---
+++ diretivas-desafio/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+	<div id="app">
+		<h1>Diretivas (Desafio)</h1>
+		<hr>
+		<!-- Exercício -->
+		<!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+		<!-- <button v-quando="acao">CLIQUE AQUI</button> -->
+		<button v-quando:click="acao">CLIQUE AQUI</button>
+		<p v-quando:mouseenter="mouseEnter"
+			v-quando:mouseleave="mouseLeave">Teste mouse event</p>
+	</div>
+</template>
+
+<script>
+export default {
+	directives:{
+		'quando' : {
+			bind(el, binding){
+				// el.onclick = function(e){
+				// 	binding.value()
+				// }
+
+				//tipoi de funcao
+				const tipo = binding.arg
+				
+				//nome da funcao
+				const fn = binding.value
+
+				//chamando a funcáo que foi passada
+				el.addEventListener(tipo, fn)
+
+			}
+		}
+	},
+	methods: {
+		acao(){
+			alert('Ação executada')
+		},
+		mouseEnter(){
+			console.log('Mouse passou no texto')
+		},
+		mouseLeave(){
+			console.log('Mouse saiu do texto')
+		}
+
+	},
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+button {
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #2196F3;
+}
+</style>
--- diretivas-exercicios/src/App.vue
+++ diretivas-exercicios/src/App.vue
@@ -2,12 +2,55 @@
 	<div id="app">
 		<p v-text="'Usando diretiva v-text'"></p>
 		<p v-html="'Usando diretiva <strong>v-html</strong>'" ></p>
+		<hr>
+		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
+		<p v-destaque="cor">Usando diretiva personalizada com argumento</p>
+		<p v-destaque-local:fundo.atrasar.alternar="'lightblue'">Usando diretiva local personalizada com argumento e modificador</p>
+		<p v-destaque-local="cor">Usando diretiva local personalizada com argumento</p>
+		<p v-destaque-local.atrasar.alternar="cor">Usando diretiva local personalizada com argumento</p>
+		<p v-destaque-local:fundo.atrasar.alternar="{cor1: 'green', cor2:'red', atraso: 2000, intervalo: 200}">Usando diretiva local personalizada com valores passados por objeto</p>
+		<p v-destaque-local.atrasar="{cor1: 'green', atraso: 5000}">Usando diretiva local personalizada com valores passados por objeto</p>
 	</div>
 </template>
 
 <script>
 export default {
-	
+	directives:{
+		'destaque-local' : {
+			bind(el, binding, vnode){
+				const aplicarCor = cor => {
+					if(binding.arg === 'fundo'){
+						el.style.backgroundColor = cor
+					}else{
+						el.style.color = cor
+					}
+				}
+
+				let atraso = 0
+				if(binding.modifiers['atrasar']) atraso = binding.value.atraso
+
+				const cor1 = binding.value.cor1
+				const cor2 = binding.value.cor2
+				let corAtual = cor1
+
+				setTimeout(() => {
+					if(binding.modifiers['alternar']) {
+						setInterval(()=>{
+							corAtual = corAtual === cor1 ? cor2 : cor1
+							aplicarCor(corAtual)
+						},binding.value.interval)
+					}else{
+						aplicarCor(binding.value.cor1)
+					}	
+				}, binding.value.atraso);
+			}
+		}
+	},
+	data() {
+		return {
+			cor: 'red'
+		}
+	},
 }
 </script>
 
